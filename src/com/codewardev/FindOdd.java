04966a654d5497f174e86304ddf8f839a3f6a093
Dale Wang <dalinwanglhtz@gmail.com>
2020-01-12 10:45:10 +1100

    updated comment

--- src/com/codewardev/FindOdd.java
+++ src/com/codewardev/FindOdd.java
@@ -2,6 +2,9 @@ package com.codewardev;
 
 // https://www.codewars.com/kata/54da5a58ea159efa38000836/train/java
 
+// alternatively we can use XOR
+// // XOR will cancel out everytime you XOR with the same number so 1^1=0 but 1^1^1=1 so every pair should cancel out leaving the odd number out
+
 import java.util.Arrays;
 import java.util.function.Function;
 import java.util.stream.Collectors;
